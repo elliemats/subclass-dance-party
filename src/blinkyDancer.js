896be0c04b850f4e2d7e463bff5521117c8e48f1
MKS-students <mks.students@hackreactor.com>
2015-08-03 19:37:51 -0500

    changed all from functional to pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,10 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
 
-  this.top = top;
-  this.left = left;
-  this.timeBetweenSteps = timeBetweenSteps;
-  this.oldStep = this.step;
 }
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -12,16 +8,13 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps){
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-
 makeBlinkyDancer.prototype.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
-    var person = this;
-    person.oldStep.bind(person);
+    makeDancer.prototype.step.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    setTimeout(person.step, person.timeBetweenSteps);
-    person.$node.toggle();
+    this.$node.toggle();
 
 
   };
--- src/dancer.js
+++ src/dancer.js
@@ -8,17 +8,20 @@ var makeDancer = function(top, left, timeBetweenSteps){
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
 
+  this.step();
+  this.setPosition(top, left);
 
 };
-
 
   makeDancer.prototype.step = function(){
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    var person = this;
-    setTimeout(person.step, timeBetweenSteps);
+    var that = this;
+    setTimeout(function () {
+      that.step();
+    }, this.timeBetweenSteps);
 
-    person.step();
+
 
   };
 
@@ -27,15 +30,13 @@ var makeDancer = function(top, left, timeBetweenSteps){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
-    var person = this;
     var styleSettings = {
       top: top,
       left: left
     };
 
-    person.$node.css(styleSettings);
+    this.$node.css(styleSettings);
 
-    person.setPosition(top, left);
 
   };
 
